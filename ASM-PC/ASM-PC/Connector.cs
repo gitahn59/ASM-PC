d6dd3fbe7b76d3b7bad5d9c01fe4dbc5ab601c11
gitahn59 <gooahn59@gmail.com>
2019-08-19 15:46:14 +0900

    udp -> tcp add btn 2  click event - get android screen image

--- ASM-PC/ASM-PC/Connector.cs
+++ ASM-PC/ASM-PC/Connector.cs
@@ -13,13 +13,17 @@ namespace ASM_PC
         public string Ip { get; }
         private IPEndPoint localAddress;
 
-        public UdpClient UdpServer { get; }
+        public Socket socket { get; }
 
         public NetworkStream Stream { get; }
         public bool isCreated { get; }
 
         public MemoryStream ms;
         public Image i;
+
+        public TcpClient client;
+        public NetworkStream stream;
+
 
         public Connector()
         {
@@ -28,52 +32,54 @@ namespace ASM_PC
             this.Port = 9876;
             this.Ip = GetIPAddress();
 
-            this.localAddress = new IPEndPoint(IPAddress.Any,this.Port);
-            UdpServer = new UdpClient(this.Port);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(this.Ip), this.Port);
 
-            //this.UdpServer.Close();
+            TcpListener server = new TcpListener(ipep);
+            server.Start();
+
+            client = server.AcceptTcpClient();
+            stream = client.GetStream();
+
             ms = new MemoryStream();
         }
 
         public void GetImage()
         {
-            int num = -1;
+            byte[] bytes = new byte[4];
+            stream.Read(bytes, 0, 4);
+            
+            int length = ByteToInt(bytes);
+            bytes = new byte[20000];
 
-            while (true)
+            MemoryStream ms = new MemoryStream();
+            int read = 0;
+            while (length>0)
             {
-                // receiving data
-                byte[] dgram = this.UdpServer.Receive(ref this.localAddress);
-                
-                int count = dgram[dgram.Length - 1];
-                if (num == -1)
-                    num = count;
-                else
-                {
-                    if (num != count)
-                    {
-                        try
-                        {
-                            i = Image.FromStream(ms);
-                            break;
-                        }
-                        catch (Exception e)
-                        {
-                            i = null;
-                            break;
-                        }
-                    }else
-                    {
-                        ms.Write(dgram, 0, dgram.Length);
-                    }
-                }
-               
-                
+                //socket read
+                read = stream.Read(bytes, 0, 20000);
+                length -= read;
+                ms.Write(bytes, 0, read);
             }
+            bytes = new byte[4];
+            stream.Write(bytes, 0, 4);
 
-            //sending data
-            //this.UdpServer.Send(dgram, dgram.Length, localAddress);
+            this.i = Image.FromStream(ms);
+
         }
 
+        public int ByteToInt(byte[] bytes)
+        {
+            int num = 0;
+            num |= bytes[0];
+            num = num << 8;
+            num |= bytes[1];
+            num = num << 8;
+            num |= bytes[2];
+            num = num << 8;
+            num |= bytes[3];
+
+            return num;
+        }
 
         public static string GetIPAddress()
         {
